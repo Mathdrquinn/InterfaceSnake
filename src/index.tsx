04b75d006a7a07f1cc4ca08d68fc137c3509c612
Quinn, Brendan <Brendan.Quinn@nextgearcapital.com>
2018-04-17 13:29:09 -0400

    :tada:

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
+import logger from 'redux-logger';
+
+import App from './App';
+import './index.css';
+
+import reducers from './reducers';
+
+ReactDOM.render(
+    <Provider
+        store={createStore(reducers, {}, applyMiddleware(logger))}
+    >
+        <App />
+    </Provider>,
+    document.getElementById('root') as HTMLElement
+);
