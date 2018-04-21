b91c6e666cfa5f5002c1fde7c709fe6854430498
Quinn, Brendan <Brendan.Quinn@nextgearcapital.com>
2018-04-21 08:51:20 -0400

    Remove NextFrame Button

--- src/containers/GameButton/Component.tsx
+++ src/containers/GameButton/Component.tsx
@@ -2,17 +2,15 @@ import * as React from 'react';
 
 export interface IGameButtonProps {
     on: boolean,
-    nextFrame: () => any,
     powerOff: () => any,
     powerOn: () => any,
 }
 
-const GameButton: React.SFC<IGameButtonProps> = ({ on, nextFrame, powerOn, powerOff }) => {
+const GameButton: React.SFC<IGameButtonProps> = ({ on, powerOn, powerOff }) => {
     const flipPower = () => on ? powerOff() : powerOn();
     return (
         <div>
             <button onClick={flipPower}>{on ? 'PAUSE' : 'PLAY'}</button>
-            <button onClick={nextFrame}>Next Frame</button>
         </div>
     );
 }
--- src/containers/GameButton/Container.ts
+++ src/containers/GameButton/Container.ts
@@ -1,19 +1,17 @@
 import { connect } from 'react-redux';
-import {IAction, IState} from '../../reducers/index';
+import { IState } from '../../reducers/index';
 import GameButton, { IGameButtonProps } from './Component';
 
 interface IConnectStateProps {
     on: boolean,
 }
 interface IConnectDispatchProps {
-    nextFrame: () => any,
     powerOff: () => any,
     powerOn: () => any
 }
 export default connect(
     ({ snake }: IState): IConnectStateProps => ({ on: snake.active }),
     (dispatch: any): IConnectDispatchProps => ({
-        nextFrame: (): IAction => dispatch({ type: 'NEXTFRAME' }),
         powerOff: (): any => dispatch({ type: 'OFF' }),
         powerOn: (): any => dispatch({ type: 'ON' }),
     }),
